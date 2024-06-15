8f38bb6ae73ea9a0198d2fcefeebe64960d2cafc
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-15 09:50:24 +0600

    special test case

--- 12. articulation point.py
+++ 12. articulation point.py
@@ -41,6 +41,8 @@ def dfs(node, parent, timer=1):
     visit[node] = 1
     in_time[node] = timer
     low_time[node] = timer
+    
+    child = 0
     for child in adj[node]:
         if child == parent: continue
         if visit[child]:# back edge
@@ -48,18 +50,22 @@ def dfs(node, parent, timer=1):
             low_time[node] = min(low_time[child], low_time[node])
         else:
             timer+=1
+            child+=1
             dfs(child, node, timer)
             low_time[node] = min(low_time[node], low_time[child])
             # if in_time[node]>low_time[child]:
             #     # no back connection
-            if (not low_time[child]<in_time[node]) and 1:
+            if (not low_time[child]<in_time[node]):# and parent!=-1:# no need as ap is set
                 '''
                 - if low time is of child less than or equal to root 
                 then that means this clind is forming another subtset of graph
                 - same node can be ap for 2 different sub graph (look at node 7)
                 '''
                 ap.add(node)
-            
+    if parent == -1 and child>1:
+        ''' secrect test case where graph has only 3 nodes'''
+        ap.add(node)
+
 def find_ap():
     cc = 0
     for i in adj:
@@ -72,17 +78,3 @@ def find_ap():
         print(f"articulation point {node}")
     
 #%%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
