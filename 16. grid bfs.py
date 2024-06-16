234ae6ebd87f047eba7c445b55fb6b49f7b846ae
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-16 20:37:21 +0600

    faulty grid bfs

---
+++ 16. grid bfs.py
@@ -0,0 +1,44 @@
+#%%
+from collections import deque
+from store_graph import print_grid
+
+#%%
+(row, col) = (3, 3)
+grid = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [1, 0, 1]
+]
+# oder   ->   up,    right,   down,    left
+movement = [(-1, 0), (0, +1), (+1, 0), (0, -1)]
+
+def is_valid(x, y):
+    if x<0 or y<0 or x>=row or y>=col:
+        return False
+    return True
+
+#%%
+level = [[-1]*row for _ in range(col)]# -1 or inf
+visit = [[0]*row for _ in range(col)]
+def bfs(x, y):
+    que = deque([(x, y)])
+    visit[x][y] = 1
+    dis = 1
+    while que:
+        x, y = que.popleft()
+        # level[x][y] = dis
+        for adx, ady in movement:
+            cx = x+adx
+            cy = y+ady
+            
+            if not is_valid(cx, cy): continue
+            if visit[cx][cy]: continue
+            
+            level[cx][cy] = dis
+            visit[cx][cy] = 1
+            que.append((cx, cy))
+        dis += 1
+
+#%%
+bfs(1, 1)
+# %%
