55216a7ff38197770819c03f2478004c197d6656
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-23 09:08:56 +0600

    faulty tarjans algo

---
+++ 17. scc tarjansalgorithm.py
@@ -0,0 +1,57 @@
+#%%
+from store_graph import graph_input, draw_graph
+
+
+n=6
+inp = '''
+0 1
+1 0
+2 1
+2 0
+2 3
+3 2
+4 1
+4 3
+4 5
+5 3
+5 6
+6 4
+'''
+adj = graph_input(inp, n, strong_edge=True)
+
+# draw_graph(cache=False, strong_edge=True)
+draw_graph(strong_edge=True, seed=9077)
+#%%
+visit = [0]*(n+1)
+in_time = [-1]*(n+1)
+low_time = [-1]*(n+1)
+on_stack=[False]*(n+1)# True False
+stack = []
+cc=[0]# counter
+def dfs(node, timer=1):
+    stack.append(node)
+    visit[node] = 1
+    on_stack[node] = 1
+    low_time[node] = timer
+    in_time[node] = timer
+    
+    for child in adj[node]:
+        if visit[child]: continue
+        
+        timer+=1
+        dfs(child, timer)
+        low_time[node] = min(low_time[node], low_time[child])# maigic hapens here
+    
+    if low_time[node]==in_time[node]:# then is't not a part of cycle or root of cycle
+        # empty until all childs are removed
+        while True: # do while loop  simulation
+            child = stack.pop()
+            if node==child: break
+            on_stack[child] = False
+            low_time[child] = low_time[node]# smart move for the iddue of dfs oder
+        cc[0]+=1
+
+# %%
+dfs(0)
+print(cc[0])
+# %%
