6ff79eb6a363c585ffeacde2d8cd32ef21b25e1a
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-23 20:22:40 +0600

    topo sort

--- 13. tropologica sort.py
+++ 13. tropologica sort.py
@@ -1,4 +1,42 @@
 '''
-online solution doesn't satisfied me and 
-for now i do not find any use case of it
+print dependencies from lift towards right
+node having zero out degree sholud be the rightmost node
 '''
+#%%
+from store_graph import graph_input, draw_graph
+from collections import deque
+
+
+n=6
+inp = '''
+1 2
+3 2
+4 1
+4 3
+5 4
+6 4
+'''
+
+adj = graph_input(inp, n, strong_edge=True)
+draw_graph(strong_edge=True, seed=6291)
+# draw_graph(False, True)
+#%%
+visit = [0]*(n+1)
+stack = []
+def dfs(node):
+    visit[node] = 1
+    
+    for child in adj[node]:
+        if visit[node]: continue
+        
+        dfs(child)
+    
+    # print(node, end=" ")
+    stack.append(node)
+
+#%%
+for node in range(1, n+1):
+    if visit[node]: continue
+    dfs(node)
+print(stack[::-1])
+# %%
