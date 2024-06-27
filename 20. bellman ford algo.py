ebc1143b7bd2b6606992329cdf456664efdcda65
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-27 21:10:56 +0600

    bug fixes

--- 20. bellman ford algo.py
+++ 20. bellman ford algo.py
@@ -9,11 +9,12 @@ inp = '''
 3 4 3
 4 2 -10
 '''
+# ans = [inf, 0, -2, 5, 8]
 
 # adj is not required
 graph_input(inp, directed=True, weighted=True)
 # draw_graph(0, 1, 1)
-draw_graph(1, 0, 0, seed = 5643)
+draw_graph(1, 1, 1, seed = 5643)
 #%%
 # process input
 nodes = []
@@ -28,8 +29,9 @@ def bellman(node):
     dis[node] = 0
     for _ in range(n-1):
         for u, v, w in nodes:
-            if dis[u]+w>dis[v]:
-                dis[v] = dis[u]+w
+            nd = dis[u]+w
+            if nd<dis[v]:
+                dis[v] = nd
 
 bellman(1)
 # %%
