09a51c7ba5809a5f45182695a78f49e778a8edae
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-28 21:35:27 +0600

    subplot removed

--- temp.py
+++ temp.py
@@ -1,3 +1,4 @@
+#%%
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
@@ -106,20 +107,19 @@ G = nx.DiGraph()
 
 G.add_edges_from(edge_list)
 pos=nx.spring_layout(G, seed=5)
-fig, ax = plt.subplots()
-nx.draw_networkx_nodes(G, pos, ax=ax)
-nx.draw_networkx_labels(G, pos, ax=ax)
-# plt.show()
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_labels(G, pos)
 
 
 curved_edges = [edge for edge in G.edges() if reversed(edge) in G.edges()]
 straight_edges = list(set(G.edges()) - set(curved_edges))
-nx.draw_networkx_edges(G, pos, ax=ax, edgelist=straight_edges)
+nx.draw_networkx_edges(G, pos, edgelist=straight_edges)
 arc_rad = 0.25
-nx.draw_networkx_edges(G, pos, ax=ax, edgelist=curved_edges, connectionstyle=f'arc3, rad = {arc_rad}')
-# fig.savefig("2.png", bbox_inches='tight',pad_inches=0)
+nx.draw_networkx_edges(G, pos, edgelist=curved_edges, connectionstyle=f'arc3, rad = {arc_rad}')
 
 edge_weights = nx.get_edge_attributes(G,'w')
 curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
-my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad)
-fig.savefig("4.png", bbox_inches='tight',pad_inches=0)
+straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
+my_draw_networkx_edge_labels(G, pos, edge_labels=curved_edge_labels, rotate=False, rad = arc_rad)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=straight_edge_labels, rotate=False)
+# %%
