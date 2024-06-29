5e70c98f4c3fa28e6eb5c54cadb2619b15f1c9ee
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-06-29 23:55:40 +0600

    faulty finding agmented path

---
+++ 22. ford fulkerson.py
@@ -0,0 +1,38 @@
+#%%
+from store_graph import graph_input, draw_graph
+from collections import defaultdict
+
+
+inp = '''
+s a 7
+s d 4
+a b 5
+a c 3
+c b 3
+d a 3
+d c 2
+c t 5
+b t 8
+'''
+
+# note: though network is directed. create adj considering undirected. because of residual edge
+adj = graph_input(inp, directed=1, weighted=1, node_type=str)
+#%%
+visit = defaultdict(lambda :0)
+def find_agmented_path(node="s", agm_path = []):
+    visit[node]=1
+    agm_path.append(node)
+    if node=="t": 
+        return print(agm_path)
+    
+    for child in adj[node]:
+        if visit[child]: continue
+        
+        agm_path.append(child)
+        find_agmented_path(child)# memorised. no need to pass variable
+        agm_path.pop()
+
+find_agmented_path()
+
+
+# %%
